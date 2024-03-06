c13d132727de81ca8cb6b46243f08c0bbad1dd51
orlapez <orlapez@gmail.com>
2024-03-06 07:58:54 -0500

    Entidad Owner

---
+++ Veterinary.Shared/Entities/Owner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinary.Shared.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Document { get; set; }    
+        public string FirstName { get; set; }    
+
+        public string LastName { get; set; }
+
+        public string  FixedPhone { get; set; }
+        public string CellPhone { get; set; }
+
+        public string Email { get; set; }   
+
+        public string Address { get; set; }
+
+
+        public string FullName => $"{FirstName}{LastName}";
+
+        public string FullWithDocument => $"{FirstName}{LastName}-{Document}";
+
+
+
+    }
+}
