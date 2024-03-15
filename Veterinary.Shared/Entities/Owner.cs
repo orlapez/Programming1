f5e202f12cd42e41f661eab0e6e1c07c71bf8f7b
orlapez <orlapez@gmail.com>
2024-03-15 07:43:05 -0500

    Controller Owners

--- Veterinary.Shared/Entities/Owner.cs
+++ Veterinary.Shared/Entities/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,45 @@ namespace Veterinary.Shared.Entities
 {
     public class Owner
     {
+ 
         public int Id { get; set; }
-        public string Document { get; set; }    
-        public string FirstName { get; set; }    
 
+        [Display(Name="Documento de identidad")]
+        [MaxLength(13,ErrorMessage ="El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage ="Este campo es obligatorio!")]
+        public string Document { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [MaxLength(50, ErrorMessage = "El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
         public string LastName { get; set; }
 
+
+        [Display(Name = "Teléfono")]
+        [MaxLength(10, ErrorMessage = "El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
         public string  FixedPhone { get; set; }
+
+        [Display(Name = "Móvil")]
+        [MaxLength(10, ErrorMessage = "El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
         public string CellPhone { get; set; }
 
-        public string Email { get; set; }   
+        [Display(Name = "Email")]
+        [MaxLength(50, ErrorMessage = "El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        [EmailAddress(ErrorMessage ="Email inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(100, ErrorMessage = "El {0} no puede tener más de {1} caracteres")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
 
         public string Address { get; set; }
 
