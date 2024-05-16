10e4c02fdfe40669c4c6fb64f9e4c55e3698ab69
orlapez <orlapez@gmail.com>
2024-05-16 12:11:23 -0500

    Add project files.

---
+++ Veterinary.API/Helpers/UserHelper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+using Veterinary.API.Data;
+using Veterinary.Shared.DTOs;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Helpers
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+        public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        
+        public async Task<IdentityResult> AdduserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public async Task AddUserToRoleAsync(User user, string roleName)
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
+
+        public async Task CheckRoleAsync(string roleName)
+        {
+            var roleExists = await _roleManager.RoleExistsAsync(roleName);
+            if (!roleExists)
+            {
+                await _roleManager.CreateAsync(new IdentityRole
+                {
+                    Name = roleName
+                });
+            }
+        }
+
+        public async Task<User> GetUserAsync(string email)
+        {
+            var user = await _context.Users
+                
+                .FirstOrDefaultAsync(x => x.Email == email);
+            return user!;
+        }
+
+        public async Task<bool> IsUserInRoleAsync(User user, string roleName)
+        {
+            return await _userManager.IsInRoleAsync(user, roleName);
+        }
+
+        public async Task<SignInResult> LoginAsync(LoginDTO model)
+        {
+            return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+    }
+}
